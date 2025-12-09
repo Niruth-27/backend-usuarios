247f372ea6405288d229aa8c394db0508f9a8240
Niruth <murganicolas524@gmail.com>
2025-12-09 16:31:03 -0500

    Eliminar server.js y establecer app.js como entrada

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 // Esquema del usuario
 const usuarioSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
-  correo: { type: String, required: true, unique: true },
+  correo: { type: String, required: true },
   edad: { type: Number, required: true }
 });
 
 // Exportar el modelo
 module.exports = mongoose.model("Usuario", usuarioSchema);
+
